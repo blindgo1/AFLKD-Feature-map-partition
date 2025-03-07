89089d7760f43468cd3a52a5bb3d93b100113f40
blindgo1 <121529356+blindgo1@users.noreply.github.com>
2025-03-07 13:08:34 +0800

    Add files via upload

---
+++ Feature-map-partition/no_split/network2/model.py
@@ -0,0 +1,80 @@
+import time
+
+import torch
+import torch.nn as nn
+
+#part1
+class VGG16Small_part1(nn.Module):
+    def __init__(self):
+        super(VGG16Small_part1, self).__init__()
+        # Conv Block 1
+
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1,padding=1)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
+        self.relu3 = nn.ReLU(inplace=True)
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+
+    def forward(self, x):
+        for name, layer in self.named_children():
+            start_time = time.time()
+            x = layer(x)
+            end_time = time.time()
+            print(name, end_time - start_time)
+        # x = self.conv1(x)
+        # x = self.relu1(x)
+        # x = self.conv2(x)
+        # x = self.relu2(x)
+        # x = self.maxpool1(x)
+        return x
+#part2
+class VGG16Small_part2(nn.Module):
+    def __init__(self):
+        super(VGG16Small_part2, self).__init__()
+        # Conv Block 2
+        self.conv4 = nn.Conv2d(32, 48, kernel_size=3, padding=1)
+        self.relu4 = nn.ReLU(inplace=True)
+        self.conv5 = nn.Conv2d(48, 48, kernel_size=3, padding=1)
+        self.relu5 = nn.ReLU(inplace=True)
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+    def forward(self, x):
+        for name, layer in self.named_children():
+            start_time = time.time()
+            x = layer(x)
+            end_time = time.time()
+            print(name, end_time - start_time)
+        # x = self.conv3(x)
+        # x = self.relu3(x)
+        # x = self.conv4(x)
+        # x = self.relu4(x)
+        # x = self.maxpool2(x)
+        return x
+
+
+#part3
+class VGG16Small_part3(nn.Module):
+    def __init__(self, num_classes=10):
+        super(VGG16Small_part3, self).__init__()
+        self.classifier = nn.Sequential(
+        nn.Linear(48 * 56 * 56, 1024),  # Adjust input size for 32x32 input
+        nn.ReLU(inplace=True),
+        nn.Dropout(),
+        nn.Linear(1024, num_classes)
+        )
+
+    def forward(self, x):
+        start_time = time.time()
+        x = x.view(-1)
+        # print('x:', x.shape)
+        x = self.classifier(x)
+        end_time = time.time()
+        print('fc_time:', end_time - start_time)
+        return x
+
+def create_model_part2_instance():
+    return VGG16Small_part2().to('cpu')
+def create_model_part3_instance():
+    return VGG16Small_part3().to('cpu')
---
+++ Feature-map-partition/no_split/network2/pi4_infer.py
@@ -0,0 +1,32 @@
+import os
+
+import torch
+import torch.distributed.rpc as rpc
+os.environ['GLOO_SOCKET_IFNAME'] = 'eth0'
+os.environ['TP_SOCKET_IFNAME'] = 'eth0'
+
+def training4(model_part2_rref, input_tensor):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = model_part2_rref.to_here()
+    output = model(input_tensor.to(device))
+    return output
+
+def init_rpc(rank, world_size):
+    # 初始化rpc
+    print('begin init rpc')
+    options = rpc.TensorPipeRpcBackendOptions(
+        init_method=f"tcp://192.168.3.24:29500",
+        _transports=["uv"]
+    )
+    # 当需要cuda间的传递时需要设置device_map
+    # options.set_device_map('worker1', {0: 0})
+    # 初始化 RPC，设置 rank 和 world_size
+    rpc.init_rpc("raspberrypi4", rank=rank, world_size=world_size, rpc_backend_options=options)
+
+    print('end init rpc')
+
+if __name__ == "__main__":
+    init_rpc(1,3)
+    #仅进行初始化并等待调用
+    # 关闭 RPC
+    rpc.shutdown()
